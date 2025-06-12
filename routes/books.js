dec171082918bf68d967815d8afe5de28d8217de
Naveensri D <142909201+Naveensri9876@users.noreply.github.com>
2025-06-13 02:01:09 +0530

    books.js

--- routes/books.js
+++ routes/books.js
@@ -1,26 +1,25 @@
-// routes/books.js
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const Book = require("../models/Book");
+const Book = require('../models/Book');
 
 // Get all books
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   const books = await Book.find();
   res.json(books);
 });
 
 // Add a new book
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   const { title, author, description } = req.body;
   const newBook = new Book({ title, author, description });
   await newBook.save();
-  res.json(newBook);
+  res.status(201).json(newBook);
 });
 
 // Delete a book
-router.delete("/:id", async (req, res) => {
+router.delete('/:id', async (req, res) => {
   await Book.findByIdAndDelete(req.params.id);
-  res.json({ message: "Book deleted" });
+  res.json({ message: 'Book deleted' });
 });
 
 module.exports = router;
